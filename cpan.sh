c822774e0794fc334680a31bbc33bc28ca2613e9
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2018-08-23 16:38:56 -0500

    update cpan.sh to automatically upload module to CPAN

--- cpan.sh
+++ cpan.sh
@@ -40,6 +40,7 @@ then
   echo $version
   mv 'voiceIt-voiceIt2-0.01.tar.gz' 'voiceIt-voiceIt2-'$version'.tar.gz'
   cpan-upload -u $PAUSEPERLUSERNAME:$PAUSEPERLPASSWORD 'voiceIt-voiceIt2-'$version'.tar.gz'
+  curl -u $GITHUBUSERNAME:$GITHUBPASSWORD -H "Content-Type: application/json" --request POST --data '{"tag_name": "'$version'", "target_commitish": "master", "name": "'$version'", "body": "", "draft": false, "prerelease": false}' https://api.github.com/repos/voiceittech/voiceit2-perl/releases
 fi
 
 
