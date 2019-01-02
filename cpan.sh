51a4ce8826891f4099a2e61272d6974d031f69e8
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2019-01-02 12:04:50 -0800

    try to fix CPAN RELEASEPATCH

--- cpan.sh
+++ cpan.sh
@@ -29,7 +29,6 @@ then
   fi
 
   version=$major'.'$minor'.'$patch
-  echo $version
   echo 'new version='$major'.'$minor'.'$patch
   mkdir -p CPAN/lib/voiceIt
   mv voiceIt2.pm CPAN/lib/voiceIt
@@ -39,11 +38,8 @@ then
   perl Makefile.PL
   make dist
 
-  echo "ls"
-  ls
-  cpan install CPAN-Uploader-0.103013::cpan-upload
-  which cpan-upload
+  # cpan install CPAN-Uploader-0.103013::cpan-upload
+  cpan CPAN::Uploader::cpan-upload
   # curl -u $GITHUBUSERNAME:$GITHUBPASSWORD -H "Content-Type: application/json" --request POST --data '{"tag_name": "'$version'", "target_commitish": "master", "name": "'$version'", "body": "", "draft": false, "prerelease": false}' https://api.github.com/repos/voiceittech/VoiceIt2-Perl/releases
   cpan-upload -u $PAUSEPERLUSERNAME -p $PAUSEPERLPASSWORD -d 'voiceIt-voiceIt2-'$version 'voiceIt-voiceIt2-'$version'.tar.gz'
-  echo "PERL PAUSE SUCCESS"
 fi
