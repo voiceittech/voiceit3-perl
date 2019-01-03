ab10cb72ffebb742547031824840432a22126037
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2019-01-03 14:01:29 -0600

    update cpan.sh RELEASEMINOR

--- cpan.sh
+++ cpan.sh
@@ -5,8 +5,7 @@ version=$(echo $(curl -s https://api.github.com/repos/voiceittech/VoiceIt2-Perl/
 set -- $version
 major=$1
 minor=$2
-patch=$3
-echo 'old version='$major'.'$minor'.'$patch
+echo 'old version='$major'.'$minor
 
 if [[ $commit = *"RELEASE"* ]];
 then
@@ -15,31 +14,31 @@ then
   then
     major=$(($major+1))
     minor=0
-    patch=0
   elif [[ $commit = *"RELEASEMINOR"* ]];
   then
     minor=$(($minor+1))
-    patch=0
   elif [[ $commit = *"RELEASEPATCH"* ]];
   then
-    patch=$(($patch+1))
+    echo "Patch release not available in Perl due to the expected behavior used in h2xs" 1>&2
+    exit 1
   else
     echo "Must specify RELEASEMAJOR, RELEASEMINOR, or RELEASEPATCH in the title." 1>&2
     exit 1
   fi
 
-  version=$major'.'$minor'.'$patch
-  echo 'new version='$major'.'$minor'.'$patch
-  mkdir -p CPAN/lib/voiceIt
-  mv voiceIt2.pm CPAN/lib/voiceIt
-  cd CPAN
-  h2xs -AX --skip-exporter --use-new-tests voiceIt::voiceIt2 -v $version
-  cd voiceIt-voiceIt2
-  perl Makefile.PL
-  make dist
-
-  # cpan install CPAN-Uploader-0.103013::cpan-upload
-  cpan CPAN::Uploader
+  version=$major'.'$minor'
+  echo 'new version='$major'.'$minor'
+  # mkdir -p CPAN/lib/voiceIt
+  # cp voiceIt2.pm CPAN/lib/voiceIt
+  # cd CPAN
+  # h2xs -AX --skip-exporter --use-new-tests voiceIt::voiceIt2 -v $version
+  # cd voiceIt-voiceIt2
+  # perl Makefile.PL
+  # make dist
+#
+  # cpan CPAN::Uploader
   # curl -u $GITHUBUSERNAME:$GITHUBPASSWORD -H "Content-Type: application/json" --request POST --data '{"tag_name": "'$version'", "target_commitish": "master", "name": "'$version'", "body": "", "draft": false, "prerelease": false}' https://api.github.com/repos/voiceittech/VoiceIt2-Perl/releases
-  cpan-upload -u $PAUSEPERLUSERNAME -p $PAUSEPERLPASSWORD -d 'voiceIt-voiceIt2-'$version 'voiceIt-voiceIt2-'$version'.tar.gz'
+  # echo "ls"
+  # ls
+  # cpan-upload --ignore-errors -u $PAUSEPERLUSERNAME -p $PAUSEPERLPASSWORD -d 'voiceIt-voiceIt2-'$version 'voiceIt-voiceIt2-'$version'.tar.gz'
 fi
